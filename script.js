c0ae7709b683b7a618ee2ffc1774da6612e1983e
saffronfarmer79 <156417214+saffronfarmer79@users.noreply.github.com>
2024-01-12 05:51:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-	
-console.log("DOM content loaded.");
 	const movies = [
 		"It's A Wonderful Life",
-		"Catch Me If You Can"
-		"Pan's Labyrinth"
-		"Ikuru"
-		"Mishima"
-		"Taxi Driver"
-		"Spirited Away"
+		"Catch Me If You Can",
+		"Pan's Labyrinth",
+		"Ikuru",
+		"Mishima",
+		"Taxi Driver",
+		"Spirited Away",
 		];
 	const pickButton = document.getElementById('pickButton');
-	const resultElement = document.getElementbyId('result');
+	const resultElement = document.getElementById('result');
 
 	pickButton.addEventListener('click', function () {
-		console.log("Button clicked.");
-		
 		const randomIndex = Math.floor(Math.random() * movies.length);
 		const randomMovie = movies[randomIndex];
-		resultElement.textContent = 'You should watch: ${randomMovie}';
+		resultElement.textContent = "You should watch: " + randomMovie;
 	});
 });
 	
